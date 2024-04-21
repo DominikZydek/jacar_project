aee657a3636e9ee7674149a0ddfbb69c4888e00d
DominikZydek <dominikzydek@gmail.com>
2024-04-21 12:45:24 +0200

    Finished with the frontend <3

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menuDropdown = document.querySelector('.menu-dropdown');
+$(document).ready(function() {
 
-    menuToggle.addEventListener('click', function() {
-        menuDropdown.classList.toggle('active');
+    $('.logo-header').on('click', () => {
+        window.location.href = '/';
+    });
+
+    $('.logo-footer').on('click', () => {
+        window.location.href = '/';
+    });
+
+    $('.menu-toggle').on('click', () => {
+        $('.menu-dropdown').toggleClass('active');
+    });
+
+    $("#datepicker").datepicker({
+        firstDay: 1,
+        dateFormat: 'yy-mm-dd',
+        dayNamesMin: ['Nd', 'Pn', 'Wt', 'Śr', 'Cz', 'Pt', 'So'],
+        monthNames: ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień'],
+        minDate: 0,
+        maxDate: '+1M',
+        onSelect: function(dateText) {
+            $("#date").val(dateText);
+        }
     });
 });
